4b853d44e891ad5870cbd2dfa1caabc7b98ed9a8
zasasamar <92506162+zasasamar2129@users.noreply.github.com>
2024-07-29 08:22:25 +0330

    Update tiktok.py

--- mbot/plugins/tiktok.py
+++ mbot/plugins/tiktok.py
@@ -38,4 +38,4 @@ async def link_handler(Mbot, message):
             if DUMP_GROUP:
                await dump_file.copy(DUMP_GROUP)
             await m.delete()
-        await message.reply("Check out @spotify_downloa_bot(music)  @spotifynewss(Channel) \n Please Support Us By /donate To Maintain This Project")
+        await message.reply("Check out @z_downloadbot(music)  @Zpotify1(Channel) \n Please Support Us By /donate To Maintain This Project")
